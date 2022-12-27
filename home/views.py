2f133f81aac9d8e19247607b0d44ef0f02e50980
Michael Freitas <michael_rfreitas@hotmail.com>
2022-12-27 19:24:14 +0000

    code: added allauth pages and messages pages with base.html

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     """ A view to return the home page """
     return render(request, 'home/home.html')
+
+
+def contact(request):
+    """ A view to return the contact page """
+    return render(request, 'home/contact.html')
