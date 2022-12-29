b3fe374621a884a345a1e8b4f88fc72fe2b26fe9
Michael Freitas <michael_rfreitas@hotmail.com>
2022-12-29 11:56:52 +0000

    fix: heroku not working

--- familio/urls.py
+++ familio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('members/', include('member.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
