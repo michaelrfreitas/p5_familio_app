9d2aa077a2128808691c254db2fb92b10f81c8c6
Michael Freitas <michael_rfreitas@hotmail.com>
2022-12-30 11:21:21 +0000

    code: blog urls and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@login_required(redirect_field_name='account_login')
+def blog(request):
+    """ A view to return the blog page """
+    return render(request, 'blog/blog.html')
+
+
+@login_required(redirect_field_name='account_login')
+def pictures(request):
+    """ A view to return the pictures page """
+    return render(request, 'blog/pictures.html')
