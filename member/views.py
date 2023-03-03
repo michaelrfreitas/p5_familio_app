a55db189563eeb53d8f191a11aca6bcf0013f4da
Michael Freitas <michael_rfreitas@hotmail.com>
2023-03-03 13:34:02 +0000

    fix: troubleshooting for familytree issue

--- member/views.py
+++ member/views.py
@@ -158,8 +158,6 @@ def tree(request):
     out_file.write('')
     json.dump(data, out_file)
     out_file.close()
-    print(out_file)
-    print(file)
     return render(request, 'member/tree.html')
 
 
