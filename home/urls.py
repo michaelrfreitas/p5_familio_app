d6bbb95bcaa2e5d5cd19dab2150bded321be6d19
Michael Freitas <michael_rfreitas@hotmail.com>
2023-03-07 01:31:11 +0000

    code: urls to sitemap and robots

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
+    path('sitemap.xml', views.sitemap),
+    path('robots.txt', views.robots),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def home(request):
@@ -9,3 +9,17 @@ def home(request):
 def contact(request):
     """ A view to return the contact page """
     return render(request, 'home/contact.html')
+
+
+def sitemap(request):
+    f = open('sitemap.xml', 'r')
+    file_content = f.read()
+    f.close()
+    return HttpResponse(file_content, content_type="text/xml")
+
+
+def robots(request):
+    f = open('robots.txt', 'r')
+    file_content = f.read()
+    f.close()
+    return HttpResponse(file_content, content_type="text/plain")
